c8cceffa274f9fd810b3488f06cb26b3214e0f43
tomakela <9822663+tomakela@users.noreply.github.com>
2026-07-11 10:58:13 +0300

    Support inventory item combinations

--- src/script_parser.c
+++ src/script_parser.c
@@ -268,6 +268,23 @@ static void yaat_parse_entity(YaatScriptPackage *package, YaatScriptCursor *curs
     yaat_match_token(cursor, SCRIPT_TOKEN_RIGHT_BRACE);
 }
 
+static YaatRoom *yaat_parse_inventory_room(YaatScriptPackage *package)
+{
+    int i;
+    YaatRoom *room;
+
+    for (i = 0; i < package->room_count; ++i) {
+        if (strcmp(package->rooms[i].id, "inventory") == 0) return &package->rooms[i];
+    }
+    if (package->room_count >= YAAT_MAX_ROOMS) return 0;
+    room = &package->rooms[package->room_count++];
+    memset(room, 0, sizeof(*room));
+    parser_copy(room->id, sizeof(room->id), "inventory", strlen("inventory"));
+    parser_copy(room->label, sizeof(room->label), "Inventory", strlen("Inventory"));
+    room->color = 0x00101018UL;
+    return room;
+}
+
 static void yaat_parse_room(YaatScriptPackage *package, YaatScriptCursor *cursor)
 {
     YaatRoom *room;
@@ -314,6 +331,11 @@ int yaat_parse_script_text_into(YaatScriptPackage *package, const char *source)
                 yaat_script_package_set_var_value(package, var_name, &value);
             }
         } else if (token->type == SCRIPT_TOKEN_KEYWORD_ROOM) yaat_parse_room(package, &cursor);
+        else if (token->type == SCRIPT_TOKEN_KEYWORD_OBJECT) {
+            YaatRoom *inventory_room = yaat_parse_inventory_room(package);
+            if (inventory_room != 0) yaat_parse_entity(package, &cursor, inventory_room, YAAT_ENTITY_OBJECT);
+            else if (yaat_peek(&cursor)->type == SCRIPT_TOKEN_LEFT_BRACE) { yaat_advance_token(&cursor); yaat_skip_block(&cursor); }
+        }
         else if (token->type == SCRIPT_TOKEN_KEYWORD_EVENT || yaat_token_is(token, "proc")) yaat_parse_event(package, &cursor, package->global_events, &package->global_event_count);
         else if (yaat_peek(&cursor)->type == SCRIPT_TOKEN_LEFT_BRACE) { yaat_advance_token(&cursor); yaat_skip_block(&cursor); }
     }
